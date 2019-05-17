bd60ec7d611221b55b9f436563a52c07cd0a49b0
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-17 18:44:29 -0500

    created course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,27 +1,33 @@
 class CoursesController < ApplicationController
     def new
-
+        @course = Course.new
     end
 
     def index
-
+        @courses = Course.all
     end
 
     def show
-
+        @course = Course.find_by(id: params[:id])
     end
 
     def edit
-
+        @course = Course.find_by(id: params[:id])
     end
 
     def create
+        @course = current_user.courses.build(course_params)
 
+        if @course.save
+            redirect_to course_path(@course)
+        else
+            render :new
+        end
     end
 
     private
 
     def course_params
-
+        params.require(:course).permit(:title, :description, :time, :category, :action_id, action_attributes: [:step, :description, :time])
     end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -8,4 +8,9 @@ class Course < ApplicationRecord
     validates :description, presence: true
 #Scoping
     scope :highest_rated, -> { where("course_rating > 4") }
+
+    def action_attributes=(attributes)
+        action = Action.find_or_create_by(attributes)
+        self.action = action if action.valid? || !self.action
+    end
 end
