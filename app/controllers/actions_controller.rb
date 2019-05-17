6e3fd7905590f41627b0ba81962ec68b6ab996bd
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-17 18:26:47 -0500

    created views for action

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,2 +1,26 @@
 class ActionsController < ApplicationController
+    def new
+        @action = Action.new
+    end
+
+    def create
+        #make current course, and create a new action related to it.
+        @action = current_course.actions.build(action_params)
+
+        if @action.save
+            redirect_to  action_path(@action)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @pet = Pet.find_by(id: params[:id])
+    end
+
+    private
+
+        def action_params
+            params.require(:action).permit(:step, :time, :description)
+        end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def current_course
+        @current_course ||= Course.find_by_id(session[:course_id])
+    end
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
 end
