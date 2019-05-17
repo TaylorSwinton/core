00fd3cb7c4bd5ee32ab913067282b183ee393288
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-16 20:41:17 -0500

    created scopes

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,8 +1,15 @@
 class Action < ApplicationRecord
+#Associations
     belongs_to :user
     belongs_to :course
-
+#Validations
     validates :step, presence: true
     validates :description, presence: true
     validates :category, presence: true
+#Scoping
+    # breathing, thinking, writing, reading 
+    scope :breathing, -> {where(category: 'breathing')}
+    scope :thinking, -> {where(category: 'thinking')}
+    scope :writing, -> {where(category: 'writing')}
+    scope :reading, -> {where(category: 'reading')}
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,11 @@
 class Course < ApplicationRecord
+#Associations
     has_many :actions
     has_many :users, through: :actions
-
+#Validations
     validates :title, presence: true
     validates :category, presence: true
     validates :description, presence: true
+#Scoping
+    scope :highest_rated, -> { where("course_rating > 4") }
 end
