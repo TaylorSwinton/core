c8036d0c1bce842ac0bb7386ce550e21db901950
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-16 21:29:49 -0500

    created secure password and commented correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+#Associations
     has_many :actions
     has_many :courses, through: :actions
-
+#Validations
     validates :name, presence: true
     validates :password_digest, presence: true
     validates :email, uniqueness: true
