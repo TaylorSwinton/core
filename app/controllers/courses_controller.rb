1ae83dd6c4cf69edc89b7d102fa62743e7ce47d9
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-21 12:27:41 -0500

    created edit and delete for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,5 +7,31 @@ class CoursesController < ApplicationController
     def show
         @course = Course.find_by(id: params[:id])
     end
+
+    def destroy
+        @course = Course.find_by(id: params[:id])
+        @course.destroy
+        redirect_to courses_path
+    end
+    
+    def edit
+        @course = Course.find_by(id: params[:id])
+    end
+
+    def update
+        @course = Course.find_by(id: params[:id])
+        if @course.update(course_params)
+          redirect_to course_path(@course)
+        else
+          render :edit
+        end
+    end
+
+
+    private
+        
+        def course_params
+            params.require(:course).permit(:title, :description, :time, :category)
+        end
     
 end
