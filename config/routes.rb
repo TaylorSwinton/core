9496af3146e7e7ead9c463763510ea96b7979285
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-21 12:02:37 -0500

    created nested show, iterator for steps on course

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 #Nested Routes
  resources :courses do
-    resources :steps, only: [:create, :new, :index]
+    resources :steps, only: [:create, :new, :index, :show]
  end
 
  resources :users do
